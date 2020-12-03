92559458c5c26c792e9e27ef6419016450954b08
Quang Pham <imminhpham+q3481@gmail.com>
2020-12-03 15:40:44 -0500

    prep for merge

--- src/main/java/com/mnp/store/api/controllers/ProductApi.java
+++ src/main/java/com/mnp/store/api/controllers/ProductApi.java
@@ -1,44 +1,9 @@
 package com.mnp.store.api.controllers;
 
-import java.util.List;
-import java.util.Optional;
-
-import com.mnp.store.contracts.catalogs.ProductService;
-import com.mnp.store.domain.catalogs.Product;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class ProductApi {
-
-    private final ProductService productservice;
-
-    public ProductApi(ProductService productService) {
-        this.productservice = productService;
-    }
-
-    @PostMapping("/product")
-    public void addProduct(String isbn, String title, int price, String author, String category) {
-        productservice.addProduct(isbn, title, price, author, category);
-    }
-
-    @GetMapping("/product")
-    public List<Product> listProducts () {
-        return productservice.listAllProducts();
-    }
-
-    @GetMapping("/product/{isbn}")
-    public Optional <Product> productByISBN (String isbn) {
-        return productservice.getProductByISBN(isbn);
-    }
-
-    @GetMapping("/product/{category}")
-    public Optional <Product> productByCategory (String category) {
-        return productservice.getProductByGategory(category);
-    }
-
 }
--- src/main/java/com/mnp/store/contracts/catalogs/ProductService.java
+++ src/main/java/com/mnp/store/contracts/catalogs/ProductService.java
@@ -1,16 +1,4 @@
 package com.mnp.store.contracts.catalogs;
 
-import com.mnp.store.domain.catalogs.Product;
-
-import java.util.List;
-import java.util.Optional;
 public interface ProductService {
-
-    void addProduct (String isbn, String title, int price, String author, String category);
-
-    List<Product> listAllProducts ();
-
-    Optional<Product> getProductByISBN (String isbn);
-
-    Optional<Product> getProductByGategory (String category);
 }
